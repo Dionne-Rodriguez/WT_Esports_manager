f086f1fca4293f67155a2d58644c22de0005dadc
Dionne <dionne.r.9@gmail.com>
2025-06-18 13:54:20 -0400

    fix wrong type on options

--- TeamScrimScheduler.js
+++ TeamScrimScheduler.js
@@ -87,11 +87,11 @@ const commands = [
         .setDescription("Number of rounds played on each selected map")
         .setRequired(true)
         .addChoices(
-          { name: "1", value: "1" },
-          { name: "2", value: "2" },
-          { name: "3", value: "3" },
-          { name: "4", value: "4" },
-          { name: "5", value: "5" }
+          { name: "1", value: 1 },
+          { name: "2", value: 2 },
+          { name: "3", value: 3 },
+          { name: "4", value: 4 },
+          { name: "5", value: 5 }
         )
     )
     .addIntegerOption((opt) =>
